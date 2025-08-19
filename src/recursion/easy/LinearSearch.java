ae6f646bad5e86e41e66376b770748a4dfa0e320
rahulraj0402 <rahul3004rt@gmail.com>
2025-08-19 23:36:53 +0530

    adding all the index which is equal to target

--- src/recursion/easy/LinearSearch.java
+++ src/recursion/easy/LinearSearch.java
@@ -1,10 +1,23 @@
 package recursion.easy;
 
+import java.util.ArrayList;
+
 public class LinearSearch {
     public static void main(String[] args) {
-        int []arr = {1 , 3 , 4 , 8 , 9 , 2};
+        int []arr = {1 , 3 , 4 , 8 ,8, 9 , 2};
         System.out.println(solve(arr, 20, 0));
         System.out.println(findIndex(arr,8 , 0));
+        addAllTheIndex(arr, 8,0);
+        System.out.println(list);
+    }
+
+    static ArrayList<Integer> list = new ArrayList<>();
+    public static void addAllTheIndex(int []arr , int target , int index){
+        if (index == arr.length - 1) return;
+        if (arr[index] == target) {
+            list.add(index);
+        }
+        addAllTheIndex(arr, target , index+1);
     }
 
     public static boolean solve(int []arr , int target , int index){
